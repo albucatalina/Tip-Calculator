65d991e93667689accc527f609890dea9ab43faa
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-04-06 17:24:00 +0300

    Show the tip value
    Show the tip value based on the amount input from the user

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -91,7 +91,7 @@ fun TipTimeLayout() {
                 .padding(bottom = 32.dp)
                 .fillMaxWidth())
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
